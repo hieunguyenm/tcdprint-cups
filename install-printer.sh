e0fa0d1cf9de5db34799a89e2a3fecc467581c6c
Hieu Nguyen <hieunguyenvn1@gmail.com>
2018-04-06 21:56:18 +0100

    Add driver and fallback command

--- install-printer.sh
+++ install-printer.sh
@@ -2,7 +2,8 @@
 
 url="http://134.226.101.24/ValidationHandlers/AccounterHandler.ashx?username=$1&password=$2"
 name="TCD_Mobile"
-ppdPath="drv:///hpcups.drv/hp-color_laserjet_cm6040_mfp.ppd"
+drvPath="drv:///hpcups.drv/hp-color_laserjet_cm6040_mfp.ppd"
+ppdPath="hp-color_laserjet_cm6040_mfp.ppd"
 printPrefix="http://134.226.101.24/ipp/TCD_Mobile"
 printerArgs="job-sheets=none -o media=iso_a4_210x297mm -o sides=one-sided"
 
@@ -18,7 +19,21 @@ else
     echo "CUPS is not installed. Please install CUPS to add printers."
   else
     IFS=':' read -r -a array <<< "$response"
-    lpadmin -p $name -m $ppdPath -v $printPrefix/${array[0]}/${array[1]} -o $printerArgs -E
+    lpadmin -p $name -m $drvPath -v $printPrefix/${array[0]}/${array[1]} -o $printerArgs -E
+
+		rc=$?;
+		if [[ $rc != 0 ]]
+		then
+			lpadmin -p $name -P $ppdPath -v $printPrefix/${array[0]}/${array[1]} -o $printerArgs -E
+
+			rc=$?
+			if [[ $rc != 0 ]]
+			then
+				echo "Cannot install printer configuration due to unknown error."
+				exit 1
+			fi
+		fi
+
     echo "TCD_Mobile printer installation completed."
   fi
 fi
