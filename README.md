57313ff09809837f8a40d215cddba68753bd66d0
Hieu Nguyen <hieunguyenvn1@gmail.com>
2018-04-06 12:01:43 +0100

    Add files

---
+++ README.md
@@ -0,0 +1,18 @@
+# tcdprint-cups
+
+Installation script for TCD_Mobile Datapac printers
+
+This script is for installing TCD_Mobile printing configuration on Linux machines. It should work with any machine which has `cups` installed but this is not tested.
+
+## Prerequisites
+
+* `cups`
+  * Debian/Ubuntu: `sudo apt install cups`
+  * Arch Linux: `sudo pacman -S cups`
+  * If your distribution is not listed here, you can easily search it for online.
+
+## Executing the script
+
+1. Allow executable permissions: `chmod +x install-printer.sh`
+
+2. Run script: `./install-printer.sh`
---
+++ install-printer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+url="http://134.226.101.24/ValidationHandlers/AccounterHandler.ashx?username=$1&password=$2"
+name="TCD_Mobile"
+ppdPath="drv:///hpcups.drv/hp-color_laserjet_cm6040_mfp.ppd"
+printPrefix="http://134.226.101.24/ipp/TCD_Mobile"
+printerArgs="job-sheets=none -o media=iso_a4_210x297mm -o sides=one-sided"
+
+response=$(curl -s $url)
+
+if [[ -z $response ]]
+then
+  echo "Invalid username/PIN."
+  exit
+else
+  if [[ -z $(command -v lpadmin) ]]
+  then
+    echo "CUPS is not installed. Please install CUPS to add printers."
+  else
+    IFS=':' read -r -a array <<< "$response"
+    lpadmin -p $name -m $ppdPath -v $printPrefix/${array[0]}/${array[1]} -o $printerArgs -E
+    echo "TCD_Mobile printer installation completed."
+  fi
+fi
